beff622eef4bfa297083e9629b2ee2ea7eb463a3
mb <mb@clearmatics.com>
2018-12-17 17:06:42 +0100

    small refactor

--- Runner.rb
+++ Runner.rb
@@ -2,16 +2,16 @@ require './Buildings.rb'
 
 @wine01 = Winnery.new
 
-def user_cmd_matrix
+def cmd_matrix
 	case gets.chomp
 	when "np" # new plant
-		@wine01.new_plant(gets.chomp, 10, 1, 1) # Name / HP / Age / Leaves
+		@wine01.new_plant(gets.chomp, rand(1..10), 1, 1) # Name / HP / Age / Leaves
 	when 'nw' # new worker
-		@wine01.new_worker(gets.chomp, 100, 17) # Name / HP / Age
+		@wine01.new_worker(gets.chomp, 17) # Name / Age
 	when "lp" # list plants
 		puts @wine01.plants_stats
 	when 'lw' # list workers
-		puts @wine01.worker_stats
+		puts @wine01.workers_stats
 	when 'grow' # push time forward 1day
 		puts @wine01.cycle_day
 	when 'q' # quit / break main loop
@@ -24,5 +24,5 @@ end
 # main loop
 $i = 1
 while $i > 0  do
-	user_cmd_matrix
+	cmd_matrix
 end
